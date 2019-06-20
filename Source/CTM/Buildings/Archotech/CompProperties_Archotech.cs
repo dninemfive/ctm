a30393d0e6806334fe63eaa3e8eb7e5d8f7eb235
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-06-20 15:56:50 -0700

    initial work on archotech

---
+++ Source/CTM/Buildings/Archotech/CompProperties_Archotech.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace D9CTM
+{
+    class CompProperties_Archotech : CompProperties
+    {
+        public IntRange TicksPerResearchPulse;
+        public IntRange TicksPerHostilePulse;
+        public IntRange HostilityAgeRange;
+        public IntRange HostilityEndRange;
+
+        public CompProperties_Archotech()
+        {
+            base.compClass = typeof(CompArchotech);
+        }
+    }
+}
