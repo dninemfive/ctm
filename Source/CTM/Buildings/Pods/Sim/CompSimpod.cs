6c4c506e95d6d775c554080d920e64c9369bef6e
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-08-09 12:43:54 -0700

    simpod balance tweak

--- Source/CTM/Buildings/Pods/Sim/CompSimpod.cs
+++ Source/CTM/Buildings/Pods/Sim/CompSimpod.cs
@@ -13,7 +13,8 @@ namespace D9CTM
         public List<SkillDef> skills => Props.SkillsToTrain;
         public float xp => Props.XpPerTick;
         public bool outdoors => Props.SatisfiesOutdoors;
-        public Building_Pod par;
+        public Building_Pod par;        
+        private const float JoyFactor = JoyTunings.BaseJoyGainPerHour / GenDate.TicksPerHour;
         public List<Pawn> pawns //should only ever have one pawn, this is just in case
         {
             get
@@ -27,6 +28,34 @@ namespace D9CTM
                 return ret;
             }
         }
+        public Pawn pawn
+        {
+            get
+            {
+                if (pawns.Count <= 0) return null;
+                return pawns.ElementAt(0);
+            }
+        }
+        public float FactorFromPassion(Passion p)
+        {
+            switch (p)
+            {
+                case Passion.None: return 0.75f;
+                default:
+                case Passion.Minor: return 1f;
+                case Passion.Major: return 1.25f;
+            }
+        }
+
+        public float JoyFactorFromInterest
+        {
+            get
+            {
+                float sum = 0f;
+                foreach(SkillDef sd in skills) sum += FactorFromPassion(pawn.skills.GetSkill(sd).passion);
+                return sum / skills.Count;
+            }
+        }
 
         public override void PostSpawnSetup(bool respawningAfterLoad)
         {
@@ -38,27 +67,33 @@ namespace D9CTM
         public override void CompTick()
         {
             base.CompTick();
-            LearnSkills(1);
-            GainJoy(1);
+            if (pawn != null)
+            {
+                LearnSkills(1);
+                GainJoy(1);
+            }
             if (ShouldEject) par.EjectContents();
         }
 
         public override void CompTickRare()
         {
             base.CompTick();
-            LearnSkills(250);
-            GainJoy(250);
+            if (pawn != null)
+            {
+                LearnSkills(250);
+                GainJoy(250);
+            }
             if (ShouldEject) par.EjectContents();
         }
 
         public void LearnSkills(int ticks)
         {
-            foreach (Pawn p in pawns) foreach (SkillDef sd in skills) p.skills.Learn(sd, xp * ticks, false);
+            foreach (SkillDef sd in skills) pawn.skills.Learn(sd, xp * ticks, false);
         }
 
         public void GainJoy(int ticks)
         {
-            foreach(Pawn p in pawns) p.needs.joy.GainJoy((parent.def.GetStatValueAbstract(StatDefOf.JoyGainFactor) * 0.36f) / (2500f/ticks), parent.def.building.joyKind);
+            pawn.needs.joy.GainJoy(parent.def.GetStatValueAbstract(StatDefOf.JoyGainFactor) * JoyFactor * JoyFactorFromInterest * ticks, parent.def.building.joyKind);
         }
 
         public bool ShouldEject
