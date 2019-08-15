c31d6d07b2e2a5a6fed34825a1a4147014d42ac6
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-08-15 09:41:04 -0700

    further work on asteroid miner events

--- Source/CTM/Buildings/Satellite/AsteroidMining/IncidentWorker_AsteroidMinerReturns.cs
+++ Source/CTM/Buildings/Satellite/AsteroidMining/IncidentWorker_AsteroidMinerReturns.cs
@@ -7,7 +7,31 @@ using RimWorld;
 
 namespace D9CTM
 {
-    class IncidentWorker_AsteroidMinerReturns : IncidentWorker
+    class IncidentWorker_AsteroidMinerReturnsFrag : IncidentWorker
+    {
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            Map map = (Map)parms.target;
+            List<Thing> things = CTMDefOf.AsteroidMinerReturns.root.Generate();
+            IntVec3 intVec = DropCellFinder.RandomDropSpot(map);
+            DropPodUtility.DropThingsNear(intVec, map, things, 110, false, true, true);
+            Find.LetterStack.ReceiveLetter("LetterLabelCargoPodCrash".Translate(), "CargoPodCrash".Translate(), LetterDefOf.PositiveEvent, new TargetInfo(intVec, map, false), null, null);
+            return true;
+        }
+    }
+    class IncidentWorker_AsteroidMinerReturnsShac : IncidentWorker
+    {
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            Map map = (Map)parms.target;
+            List<Thing> things = CTMDefOf.AsteroidMinerReturns.root.Generate();
+            IntVec3 intVec = DropCellFinder.RandomDropSpot(map);
+            DropPodUtility.DropThingsNear(intVec, map, things, 110, false, true, true);
+            Find.LetterStack.ReceiveLetter("LetterLabelCargoPodCrash".Translate(), "CargoPodCrash".Translate(), LetterDefOf.PositiveEvent, new TargetInfo(intVec, map, false), null, null);
+            return true;
+        }
+    }
+    class IncidentWorker_AsteroidMinerReturnsCore : IncidentWorker
     {
         protected override bool TryExecuteWorker(IncidentParms parms)
         {
---
+++ Source/CTM/Buildings/Satellite/AsteroidMining/ThingSetMaker_AsteroidMining.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace D9CTM
+{
+    class ThingSetMaker_AsteroidMining : ThingSetMaker
+    {
+        private static List<ThingDef> mineables = new List<ThingDef>();
+        private static Dictionary<ThingDef, float> weightedMineables = new Dictionary<ThingDef, float>();
+
+        public List<ThingDefCountClass> guaranteedItems;
+
+        public static void Reset()
+        {
+            mineables.Clear();
+            weightedMineables.Clear();
+            mineables.AddRange(from x in DefDatabase<ThingDef>.AllDefsListForReading where (x.deepCommonality > 0f || HasMineable(x)) && x != ThingDefOf.Chemfuel select x);
+
+        }
+
+        private static bool HasMineable(ThingDef y)
+        {
+            /*
+            IEnumerable<ThingDef> things = from x in DefDatabase<ThingDef>.AllDefsListForReading where (x.building != null) && (x.building.mineableThing == y) && (x.building.mineableScatterCommonality > 0f) select x;
+            return things.Any();
+            */
+            IEnumerable<ThingDef> buildings = from x in DefDatabase<ThingDef>.AllDefsListForReading where x.building != null select x;
+            foreach (ThingDef td in buildings) if (td.building.mineableThing == y && td.building.mineableScatterCommonality > 0f) return true;
+            return false;
+        }
+
+        protected override IEnumerable<ThingDef> AllGeneratableThingsDebugSub(ThingSetMakerParams parms)
+        {
+            return mineables;
+        }
+    }
+}
