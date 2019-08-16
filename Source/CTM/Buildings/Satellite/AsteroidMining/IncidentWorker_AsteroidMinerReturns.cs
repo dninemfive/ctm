1e6e08501c73526e70fda45ec60102815aea988a
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-08-16 08:24:00 -0700

    asteroid mining events theoretically done

--- Source/CTM/Buildings/Satellite/AsteroidMining/IncidentWorker_AsteroidMinerReturns.cs
+++ Source/CTM/Buildings/Satellite/AsteroidMining/IncidentWorker_AsteroidMinerReturns.cs
@@ -12,10 +12,10 @@ namespace D9CTM
         protected override bool TryExecuteWorker(IncidentParms parms)
         {
             Map map = (Map)parms.target;
-            List<Thing> things = CTMDefOf.AsteroidMinerReturns.root.Generate();
+            List<Thing> things = CTMDefOf.D9AsteroidMiner_Frag.root.Generate();
             IntVec3 intVec = DropCellFinder.RandomDropSpot(map);
             DropPodUtility.DropThingsNear(intVec, map, things, 110, false, true, true);
-            Find.LetterStack.ReceiveLetter("LetterLabelCargoPodCrash".Translate(), "CargoPodCrash".Translate(), LetterDefOf.PositiveEvent, new TargetInfo(intVec, map, false), null, null);
+            Find.LetterStack.ReceiveLetter("D9AsteroidMinerLabel".Translate(), "D9AsteroidMinerDescFrag".Translate(), LetterDefOf.PositiveEvent, new TargetInfo(intVec, map, false), null, null);
             return true;
         }
     }
@@ -24,10 +24,10 @@ namespace D9CTM
         protected override bool TryExecuteWorker(IncidentParms parms)
         {
             Map map = (Map)parms.target;
-            List<Thing> things = CTMDefOf.AsteroidMinerReturns.root.Generate();
+            List<Thing> things = CTMDefOf.D9AsteroidMiner_Shaq.root.Generate();
             IntVec3 intVec = DropCellFinder.RandomDropSpot(map);
             DropPodUtility.DropThingsNear(intVec, map, things, 110, false, true, true);
-            Find.LetterStack.ReceiveLetter("LetterLabelCargoPodCrash".Translate(), "CargoPodCrash".Translate(), LetterDefOf.PositiveEvent, new TargetInfo(intVec, map, false), null, null);
+            Find.LetterStack.ReceiveLetter("D9AsteroidMinerLabel".Translate(), "D9AsteroidMinerDescShaq".Translate(), LetterDefOf.PositiveEvent, new TargetInfo(intVec, map, false), null, null);
             return true;
         }
     }
@@ -36,10 +36,10 @@ namespace D9CTM
         protected override bool TryExecuteWorker(IncidentParms parms)
         {
             Map map = (Map)parms.target;
-            List<Thing> things = CTMDefOf.AsteroidMinerReturns.root.Generate();
+            List<Thing> things = CTMDefOf.D9AsteroidMiner_Core.root.Generate();
             IntVec3 intVec = DropCellFinder.RandomDropSpot(map);
             DropPodUtility.DropThingsNear(intVec, map, things, 110, false, true, true);
-            Find.LetterStack.ReceiveLetter("LetterLabelCargoPodCrash".Translate(), "CargoPodCrash".Translate(), LetterDefOf.PositiveEvent, new TargetInfo(intVec, map, false), null, null);
+            Find.LetterStack.ReceiveLetter("D9AsteroidMinerLabel".Translate(), "D9AsteroidMinerDescCore".Translate(), LetterDefOf.PositiveEvent, new TargetInfo(intVec, map, false), null, null);
             return true;
         }
     }
