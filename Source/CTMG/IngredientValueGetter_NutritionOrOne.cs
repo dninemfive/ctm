4043b1bfeb55dfdde5eb453dc96d5524fcf9c0d9
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-06-22 13:29:23 -0700

    add synthleather recipes; tweak synthleather and nanoweave balance

---
+++ Source/CTMG/IngredientValueGetter_NutritionOrOne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace D9CTM
+{
+    /// <summary>
+    /// Gets nutrition from inputs, but returns 1 for non-nutritious ingredients to allow for mixing food and non-food ingredients.
+    /// 
+    /// NOTE: will not prevent the use of non-food ingredients if you select overly broad filters.
+    /// </summary>
+    public class IngredientValueGetter_NutritionOrOne : IngredientValueGetter
+    {
+        public override float ValuePerUnitOf(ThingDef t)
+        {
+            if (!t.IsNutritionGivingIngestible) return 1f;
+            return t.GetStatValueAbstract(StatDefOf.Nutrition);
+        }
+        public override string BillRequirementsDescription(RecipeDef r, IngredientCount ing)
+        {
+            return "BillRequiresNutrition".Translate(ing.GetBaseCount()) + " (" + ing.filter.Summary + ")";
+        }
+    }
+}
