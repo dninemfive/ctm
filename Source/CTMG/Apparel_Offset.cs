9bfcd4ab18dfc16a90f548abbe1c69e15cdf62dd
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-08-05 14:34:50 -0700

    ThingClass with apparel score offset from comps

---
+++ Source/CTMG/Apparel_Offset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace D9CTM
+{
+    class Apparel_Offset : Apparel
+    {
+        public override float GetSpecialApparelScoreOffset()
+        {
+            float ret = 0f;
+            foreach (ThingComp comp in base.AllComps) if (comp is CompWithApparelOffset cwao) ret += cwao.ApparelScoreOffset;
+            return ret;
+        }
+    }
+    abstract class CompWithApparelOffset : ThingComp
+    {
+        public abstract float ApparelScoreOffset { get; set; }
+    }
+}
