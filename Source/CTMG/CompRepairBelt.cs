536ed198a3b8496470963e7886581bc268a2d878
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-08-05 16:26:38 -0700

    finish:tm: CompRepairBelt

--- Source/CTMG/CompRepairBelt.cs
+++ Source/CTMG/CompRepairBelt.cs
@@ -10,20 +10,31 @@ namespace D9CTM
 {
     class CompRepairBelt : CompEMPable
     {
+        public new CompProperties_RepairBelt Props => (CompProperties_RepairBelt)base.props;
         private Pawn Pawn => (base.parent as Apparel)?.Wearer;
-        public bool IsCheapHashIntervalTick(int tick) => true;
+        public bool IsCheapIntervalTick(int interval) => (int)(Find.TickManager.TicksGame + hashOffset) % interval == 0;
         public int HPToRepairPerPulse => (int)base.parent.GetStatValue(Props.apparelScoreOffsetStat);
+        #region cheap hash interval shit
+        private int hashOffset = 0;      
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            hashOffset = parent.thingIDNumber.HashOffset();
+        }
+        #endregion cheap hash interval shit
 
         public override void CompTick()
         {
             base.CompTick();
-            if (!EMPed && IsCheapHashIntervalTick(Find.TickManager.TicksAbs)) RepairItems();
+            if (!EMPed && IsCheapIntervalTick(Props.tickInterval)) RepairItems();
         }
 
         public void RepairItems()
         {
             // I could short-circuit the IEnumerable, and I strongly considered it, but it's not worth the performance loss in cases where you'd want to iterate over the list multiple times.
             // Besides, with this, we can shuffle the order to make it seem more naturalistic.
+            // also considered healing with more than one HP at a time for cases with a lot of HP to repair and few items, but it's not worth the added complexity imo.
             List<Thing> itemsToRepair = ItemsToRepair().ToList();
             int hpLeft = HPToRepairPerPulse;
             while(hpLeft > 0 && itemsToRepair.Count > 0)
@@ -50,6 +61,12 @@ namespace D9CTM
     }
     class CompProperties_RepairBelt : CompProperties_EMPable
     {
-
+#pragma warning disable CS0649
+        public int tickInterval = 250;
+#pragma warning restore CS0649
+        public CompProperties_RepairBelt()
+        {
+            base.compClass = typeof(CompRepairBelt);
+        }
     }
 }
