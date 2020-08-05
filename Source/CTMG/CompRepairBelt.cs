43dd0fdeeadd5169e537f8894a6b84c7db1f76fa
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-08-05 16:11:51 -0700

    more work on CompRepairBelt

--- Source/CTMG/CompRepairBelt.cs
+++ Source/CTMG/CompRepairBelt.cs
@@ -11,6 +11,32 @@ namespace D9CTM
     class CompRepairBelt : CompEMPable
     {
         private Pawn Pawn => (base.parent as Apparel)?.Wearer;
+        public bool IsCheapHashIntervalTick(int tick) => true;
+        public int HPToRepairPerPulse => (int)base.parent.GetStatValue(Props.apparelScoreOffsetStat);
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (!EMPed && IsCheapHashIntervalTick(Find.TickManager.TicksAbs)) RepairItems();
+        }
+
+        public void RepairItems()
+        {
+            // I could short-circuit the IEnumerable, and I strongly considered it, but it's not worth the performance loss in cases where you'd want to iterate over the list multiple times.
+            // Besides, with this, we can shuffle the order to make it seem more naturalistic.
+            List<Thing> itemsToRepair = ItemsToRepair().ToList();
+            int hpLeft = HPToRepairPerPulse;
+            while(hpLeft > 0 && itemsToRepair.Count > 0)
+            {
+                Thing thing = itemsToRepair.RandomElement();
+                if (thing.HitPoints < thing.MaxHitPoints)
+                {
+                    thing.HitPoints++;
+                    hpLeft--;
+                }
+                else itemsToRepair = itemsToRepair.Where(x => x.HitPoints < x.MaxHitPoints).ToList();
+            }
+        }
 
         public IEnumerable<Thing> ItemsToRepair()
         {
@@ -21,5 +47,9 @@ namespace D9CTM
             Thing carriedThing = Pawn.carryTracker.CarriedThing;
             if (carriedThing.def.useHitPoints && carriedThing.HitPoints < carriedThing.MaxHitPoints) yield return carriedThing;
         }
+    }
+    class CompProperties_RepairBelt : CompProperties_EMPable
+    {
+
     }
 }
