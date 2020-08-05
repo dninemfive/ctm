6ddcad85f4d8796de3a05f512300bd0c6c4506d5
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-08-05 15:18:09 -0700

    thing

--- Source/CTMG/CompEMPable.cs
+++ Source/CTMG/CompEMPable.cs
@@ -13,6 +13,8 @@ namespace D9CTM
         public new CompProperties_EMPable Props => (CompProperties_EMPable)base.props;
         public int CooldownTicks = 0;
         public bool EMPed => CooldownTicks > 0;
+        // generally not used unless the EMP cooldown lasts long enough to leave combat but might as well
+        public override float ApparelScoreOffset => EMPed ? 0f : base.ApparelScoreOffset;
 
         public override void CompTick()
         {
@@ -22,7 +24,7 @@ namespace D9CTM
         public override void PostPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
         {
             if (Props.damageDefs.Contains(dinfo.Def)) CooldownTicks = Props.cooldownTicks;
-        }
+        }        
     }
     class CompProperties_EMPable : CompProperties_ApparelScoreOffset
     {
